f0f504b909aab03c6043a4e294ed11de28c8619e
mutebinuhu <mutebinuhu1@gmail.com>
2025-04-02 18:45:14 +0400

    core: few changes

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,3 @@
 import {Inter, Lusitana} from "next/font/google"
 export const inter = Inter({subsets:['latin'], weight:"500"})
-export const lusi = Lusitana({subsets:['latin'], weight:["400", "700"]})
+export const lusitana= Lusitana({subsets:['latin'], weight:["400", "700"]})
